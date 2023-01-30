f1d2532e0cc1ad9f804cb5fde362b089de43ca99
Itzel Villaseñor <110121112+Itzelvillg@users.noreply.github.com>
2023-01-29 21:20:55 -0600

    Add files via upload
    Con inicio de Sesion

--- resources/views/website/iniciosesion.blade.php
+++ resources/views/website/iniciosesion.blade.php
@@ -8,8 +8,44 @@
 
 @section('main')
 
-    
-    
+
+      
+  <main class="main-cuenta">
+    <h1 class="titulo"> Inicia Sesión</h1>
+
+    <div class="caja-inicio-sesion">
+
+          <div class="caja-foto">
+              <div class="caja-formulario-cuenta">
+
+                    <div class="flex-column">
+                      <label class="label-formulario" for="correo">Tu correo</label>
+                      <input class="input-formulario" id="correo" type="email" name="correo" placeholder="dudas@gmail.com">
+
+                    </div>
+                    <div  class="flex-column">
+                      <label class="label-formulario" for="contraseña">Tu contraseña</label>
+                      <input class="input-formulario" id="contraseña" type="password" name="contraseña" placeholder="tu contraseña">
+
+                    </div>
+                    <div class="botones-cuenta botones-cuenta-sesion">
+                      <button class="btn-selector">Acceder</button>
+                      <a href="{{ url('crearcuenta') }}">¿No tienes cuenta? Crea una aquí</a>
+                    </div>
+
+
+              </div>
+
+              
+
+          </div>
+
+    </div>
+
+
+  </main>
+
+
  
         
 
